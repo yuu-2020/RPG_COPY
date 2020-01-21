155c93332f7f6ce2a1efc96bc0e50e45878ea0c8
yuu-2020 <yuu.developper.2020@gmail.com>
2020-01-21 20:56:29 +0900

    Initial commit

---
+++ Assets/SelectWeapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectWeapon : MonoBehaviour
+{
+    /**
+     * 武器を選ぶためのボタン
+     */
+    Button button_1;
+    Button button_2;
+    Button button_3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ボタンのコンポーネントを取得
+        button_1 = GameObject.Find("/Canvas/Button_1").GetComponent<Button>();
+        button_2 = GameObject.Find("/Canvas/Button_2").GetComponent<Button>();
+        button_3 = GameObject.Find("/Canvas/Button_3").GetComponent<Button>();
+
+        // 最初にButtan_1を選択状態にする．
+        button_1.Select();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool clear;
+    public Text textGameClear;
+    public Text textGameNotClear;
+    public static int life;
+    private float count = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clear = false;
+        textGameClear.enabled = false;
+        textGameNotClear.enabled = false;
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player" && life==3)
+        {
+            textGameClear.enabled = true; 
+        }
+        if(collision.gameObject.tag == "Player" && life < 3)
+        {
+            textGameNotClear.enabled = true;
+            Invoke("kesu",2);    
+         }
+    }
+    void kesu()
+    {
+        textGameNotClear.enabled = false;
+    }
+}

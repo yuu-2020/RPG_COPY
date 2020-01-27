8da0917abb7251c8bdffa5142c9f77e1476893cd
yuu-2020 <yuu.developper.2020@gmail.com>
2020-01-27 15:40:33 +0900

    [未完成]持ち物を替える機能の実装
    changeEquipで武器の持ち替えを実装．
    changeButtonで押されたボタンを判別する機能を実装．
    
    その結果，まだうまくできてない．

---
+++ Assets/ChangeButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeButton : MonoBehaviour
+{
+    ChangeEquip equip = new ChangeEquip();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        equip.isButton_1 = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClick(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                equip.isButton_1 = true;
+                break;
+            case 1:
+                equip.isButton_2 = true;
+                break;
+            case 2:
+                equip.isButton_3 = true;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/ChangeEquip.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeEquip : MonoBehaviour
+{
+
+    /**
+     * 武器を選んだボタン
+     */
+    public bool isButton_1 = false;
+    public bool isButton_2 = false;
+    public bool isButton_3 = false;
+
+    [SerializeField]
+    private GameObject[] weapons;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weapons[0].SetActive(false);
+        weapons[0].SetActive(false);
+        weapons[0].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isButton_1)
+		{
+            weapons[0].SetActive(true);
+            weapons[0].SetActive(false);
+            weapons[0].SetActive(false);
+        }
+
+        if (isButton_2)
+		{
+            weapons[0].SetActive(false);
+            weapons[0].SetActive(true);
+            weapons[0].SetActive(false);
+        }
+
+        if (isButton_3)
+		{
+            weapons[0].SetActive(false);
+            weapons[0].SetActive(false);
+            weapons[0].SetActive(true);
+        }
+    }
+}
